af893aab20428ec2600a12474bb44031dcea5666
camimtzd <143425459+camimtzd@users.noreply.github.com>
2023-12-05 20:58:10 -0300

    modif

--- ProyectoDjango/tpfinal/portfolio/models.py
+++ ProyectoDjango/tpfinal/portfolio/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     descripcion = models.TextField(verbose_name="Descripción")
-    image = models.ImageField(verbose_name="Imagen")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
     created = models.DateTimeField (auto_now_add=True,verbose_name="Fecha Creacion")
     updated = models.DateTimeField (auto_now=True, verbose_name="Fecha modificacion")
     
