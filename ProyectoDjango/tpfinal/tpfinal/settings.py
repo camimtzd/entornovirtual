4ef44305bfe6bd4d4b74b04fce03cf8c4f8f4159
camimtzd <143425459+camimtzd@users.noreply.github.com>
2023-12-20 19:30:56 -0300

    Update settings.py

--- ProyectoDjango/tpfinal/tpfinal/settings.py
+++ ProyectoDjango/tpfinal/tpfinal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$x2ni4k!mdok*qa(g2oz2-olcw*uz0puwvhxqoy7$e&pstwcm='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://camiger.pythonanywhere.com/']
 
 
 # Application definition
